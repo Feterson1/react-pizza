ffe1845e6e4a37d3f8ccd15ee975fafdbc5498e6
Feterson1 <Feterson@bk.ru>
2023-03-15 21:35:41 +0600

    Pizza request for backend mockapi

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from './components/Header/Header';
 import Sort from './components/Sort/Sort';
 import Categories from './components/Categories/Categories';
@@ -6,6 +7,18 @@ import PizzaBlock from './components/PizzaBlock/PizzaBlock';
 import pizzas from '../src/assets/pizzas.json';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://6411dc076e3ca31753000a5d.mockapi.io/items')
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
+
   return (
     <div className="App">
       <div className="wrapper">
@@ -18,7 +31,7 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {pizzas.map((obj, index) => {
+              {items.map((obj, index) => {
                 return <PizzaBlock key={index} {...obj} />;
               })}
             </div>
