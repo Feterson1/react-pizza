cd9da4534c479d0d34d2241f3628c7b6ea6e4cee
Feterson1 <Feterson@bk.ru>
2023-03-17 02:31:54 +0600

    fixed layout/added cart

--- src/App.js
+++ src/App.js
@@ -8,18 +8,14 @@ import Cart from './pages/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </div>
-        </div>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </div>
   );
