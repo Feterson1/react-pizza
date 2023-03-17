94903c894ab78bab078f75d03feb7a35ee0ae90f
Feterson1 <Feterson@bk.ru>
2023-03-18 01:26:36 +0600

    Pagination and pizza search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import ReactPaginate from 'react-paginate';
 
 import Categories from '../components/Categories/Categories';
 import Sort from '../components/Sort/Sort';
 import PizzaBlock from '../components/PizzaBlock/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination/Pagination';
 
-export default function Home(props) {
+export default function Home({ searchValue }) {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
   const [sortType, setSortType] = React.useState({
     name: 'популярности',
     sortProperty: 'rating',
@@ -19,8 +22,9 @@ export default function Home(props) {
     const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
     const sortBy = sortType.sortProperty.replace('-', '');
     const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = searchValue ? `&search=${searchValue}` : '';
     fetch(
-      `https://6411dc076e3ca31753000a5d.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+      `https://6411dc076e3ca31753000a5d.mockapi.io/items?page=${currentPage}&limit=4${category}&sortBy=${sortBy}&order=${order}${search}`,
     )
       .then((res) => {
         return res.json();
@@ -30,8 +34,10 @@ export default function Home(props) {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
+  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
 
+  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
   return (
     <div className="container">
       <div className="content__top">
@@ -39,11 +45,8 @@ export default function Home(props) {
         <Sort sortValue={sortType} onChangeSortType={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-      </div>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 }
