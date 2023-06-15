16a57062ef95118d7e197ccacfcbfb3b8ea221ce
Feterson <feterson@bk.ru>
2023-06-15 18:48:31 +0600

    Separated business logic from ui

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import filterSlice from './slices/filter/filterSlice';
 import cartSlice from './slices/cart/cartSlice';
+import pizzaSlice from './slices/pizza/pizzaSlice';
 
 export const store = configureStore({
   reducer: {
     filter:filterSlice,
     cart: cartSlice,
+    pizza: pizzaSlice,
   },
 });
